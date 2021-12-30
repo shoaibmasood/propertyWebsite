de29c32ae86ae955b09c841bd3ff9dfb9fecdfd4
Salman Zia <salman.ctr563@gmail.com>
2021-12-30 03:49:14 -0800

    designed Card component

---
+++ components/Common/DropDownMenu/Card/Card.jsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import BackgroundImage from "../../../../public/sell-your-house.jpg";
+import { useStyles } from "./Card.syles";
+import Box from "@mui/material/Box";
+
+export default function CardComponentWithBackgroundImage({
+  cardTitle,
+  cardDescription,
+  buttonTitle,
+  buttonOnClick,
+}) {
+  const { cardWrapper, cardContentWrapper, cardContent } = useStyles();
+  return (
+    <Card sx={{ maxWidth: 345 }} className={cardWrapper}>
+      <Box className={cardContentWrapper}>
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant='h5'
+            component='div'
+            className={cardContent}
+          >
+            {cardTitle}
+          </Typography>
+          <Typography
+            variant='body2'
+            color='text.secondary'
+            className={cardContent}
+          >
+            {cardDescription}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size='small' onClick={buttonOnClick}>
+            {buttonTitle}
+          </Button>
+        </CardActions>
+      </Box>
+    </Card>
+  );
+}
