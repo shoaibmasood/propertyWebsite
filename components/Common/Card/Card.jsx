baa4d12f357672c17ce0093774dae1570755fecb
Shoaib Masood <shoaib_masood@rocketmail.com>
2021-12-30 17:12:13 +0500

    moved card component to common

--- components/Common/Card/Card.jsx
+++ components/Common/Card/Card.jsx
@@ -1,13 +1,11 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import BackgroundImage from "../../../../public/sell-your-house.jpg";
-import { useStyles } from "./Card.syles";
-import Box from "@mui/material/Box";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useStyles } from './Card.syles';
+import Box from '@mui/material/Box';
 
 export default function CardComponentWithBackgroundImage({
   cardTitle,
@@ -22,22 +20,22 @@ export default function CardComponentWithBackgroundImage({
         <CardContent>
           <Typography
             gutterBottom
-            variant='h5'
-            component='div'
+            variant="h5"
+            component="div"
             className={cardContent}
           >
             {cardTitle}
           </Typography>
           <Typography
-            variant='body2'
-            color='text.secondary'
+            variant="body2"
+            color="text.secondary"
             className={cardContent}
           >
             {cardDescription}
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size='small' onClick={buttonOnClick}>
+          <Button size="small" onClick={buttonOnClick}>
             {buttonTitle}
           </Button>
         </CardActions>
