3084f17b8282950cc2c122b3bc1face817424dac
Shoaib Masood <shoaib_masood@rocketmail.com>
2022-01-02 16:05:50 +0500

    added responsiveness to testimonial components for all screen sizes

--- components/Testimonial/Testimonial.styles.js
+++ components/Testimonial/Testimonial.styles.js
@@ -2,11 +2,29 @@ import { makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles({
   testimonialContainer: {
+    display: 'flex',
     marginTop: '3.8rem',
     marginBottom: '3.8rem',
     '& .slick-slider > svg': {
       color: '#ff6161',
     },
+  },
+  testimonialSlider: {
+    '& .slick-slider': {
+      '@media screen and (max-width: 325px)': {
+        maxWidth: '17rem',
+      },
+      '@media screen and (min-width: 370px)': {
+        maxWidth: '20rem',
+      },
+      '@media screen and (min-width: 700px)': {
+        maxWidth: '35rem',
+      },
+      '@media screen and (min-width: 1020px)': {
+        maxWidth: '48rem',
+      },
+    },
+    // maxWidth: '48rem',
   },
   testimonialIcon: {
     display: 'flex',
